9162be8422bb135a6a1cfe8c4cdbdfb0caa72ab3
Edivaldo_Junior <junioraraujoarau66@gmail.com>
2022-03-19 14:19:43 -0300

    first commit

---
+++ src/view.mjs
@@ -0,0 +1,12 @@
+//view manipula oque tem na tela, mais sem regra de negocios ou regras
+export default class View{
+    constructor(){
+        this.btnStart = document.getElementById('btnStart')
+        this.btnStop = document.getElementById('btnStop')
+        this.audio = document.getElementById('audio')
+    }
+
+    configureStartRecordingBtn(command) {
+        
+    }
+}
